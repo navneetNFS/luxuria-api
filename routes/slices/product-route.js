75f401ee298aae573144bd0631921ea2d7aa9c8f
navneetNFS <navneettaneja.fullstack@gmail.com>
2023-10-03 01:49:41 +0530

    update code

--- routes/slices/product-route.js
+++ routes/slices/product-route.js
@@ -2,10 +2,23 @@ const express = require('express');
 const product_route = express.Router();
 const product_ctrl = require('../../controllers/product.ctrl')
 const { authentication, authorizeRoles , isVerified } = require('../../middleware/isAuth')
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/images'); // Set the destination folder for uploaded images
+    },
+    filename: (req, file, cb) => {
+        const today = new Date();
+		const current_time = `${today.getDate()}.${today.getMonth()}.${today.getFullYear()}.${today.getHours()}.${today.getMinutes()}.${today.getSeconds()}`;
+		cb(null, current_time + '-' + file.originalname); // Set the file name
+    },
+});
+
+const upload = multer({ storage });
 
 product_route.get('/', product_ctrl.getProducts)
 product_route.get('/:prodId', product_ctrl.getSingleProduct)
-product_route.post('/create-product', authentication , authorizeRoles("admin") , isVerified(true) , product_ctrl.createProduct)
+product_route.post('/create-product' , authentication , authorizeRoles("admin") , isVerified(true) , product_ctrl.createProduct)
 product_route.put('/edit-product/:prodId', authentication , authorizeRoles("admin") , isVerified(true) , product_ctrl.editProduct)
 product_route.delete('/delete-product/:prodId', authentication , authorizeRoles("admin") , isVerified(true) , product_ctrl.deleteProduct)
 
