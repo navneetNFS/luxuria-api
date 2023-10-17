be7c3d80b95ded9e6a5ac5358b84752cfd7b422e
navneetNFS <navneettaneja.fullstack@gmail.com>
2023-10-17 23:26:00 +0530

    work on password check route

--- routes/slices/user-route.js
+++ routes/slices/user-route.js
@@ -21,7 +21,7 @@ const upload = multer({ storage });
 
 // Login User
 user_route.post('/login', user_ctrl.loginUser)
-user_route.get('/getPwd', authentication , isVerified(false) , user_ctrl.getPwd)
+user_route.get('/getPwd', authentication , isVerified(true) , user_ctrl.getPwd)
 
 // create-user
 user_route.post('/new-user', upload.single('avatar') , user_ctrl.createUser)
