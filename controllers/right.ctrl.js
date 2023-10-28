d9ceff92d01088cc19259dcc71ee8ca04bd98bb7
navneetNFS <navneettaneja.fullstack@gmail.com>
2023-10-28 15:16:59 +0530

    worked on right update api

--- controllers/right.ctrl.js
+++ controllers/right.ctrl.js
@@ -1,5 +1,5 @@
-const error = require('../middleware/error')
-const Right = require('../models/right.mdl')
+const Right = require('../models/right.mdl');
+const error = require('../middleware/error');
 module.exports.postRight = (req,res,next)=> {
     const data = req.body
     const right = new Right(data)
@@ -14,6 +14,21 @@ module.exports.getSingleRight = (req,res,next)=> {
     const email = req.params.email;
     Right.findOne({email})
     .then((data)=>{
-        res.status(201).json({success: true,rights: data.rights})
+        res.status(201).json({success: true,id:data._id,rights: data.rights})
+    }).catch((err) => error.ErrorHandler(501,err.message,res))
+}
+
+
+module.exports.updateRight = (req,res,next) => {
+    const rightId = req.params.rightId;
+    const rightData = req.body
+    console.log(rightId);
+    console.log(rightData);
+
+    Right.findOne({_id:rightId})
+    .then((data) => {
+        data.rights = rightData
+        data.save()
+        res.status(201).json({success:true,rights:data})
     }).catch((err) => error.ErrorHandler(501,err.message,res))
 }
--- routes/slices/right-route.js
+++ routes/slices/right-route.js
@@ -5,5 +5,6 @@ const { authentication, authorizeRoles , isVerified } = require('../../middlewar
 
 right_route.post('/', authentication , authorizeRoles("super-admin") , isVerified(true) , right_ctrl.postRight)
 right_route.get('/:email' ,  authentication , authorizeRoles("super-admin") , isVerified(true) , right_ctrl.getSingleRight)
+right_route.put('/:rightId' ,  authentication , authorizeRoles("super-admin") , isVerified(true) , right_ctrl.updateRight)
 
 module.exports = right_route
